3bc38e6a4c8d86528693cf181ed349b4a8ae68ee
Wil Chung <iamwil@gmail.com>
2015-06-28 20:53:00 -0700

    use redis to calculate minhash

--- minhash.rb
+++ minhash.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/ruby
 
+require 'redis'
 require 'murmurhash3'
 require 'pp'
 
@@ -70,19 +71,42 @@ module Storage
 
   module Redis
 
-    def setup
+    def setup(sets)
+      @redis = ::Redis.new
 
+      sets.each do |set|
+        @redis.del "hmhll:minhash:#{set}"
+        @redis.hmset "hmhll:minhash:#{set}", *(0...@hashes.length).map { |i| [i, 2**64] }.flatten
+      end
     end
 
     def add(data)
+      hash_val = @hashes.map { |hash| hash.call(@data_count) }
+      @data_count += 1
+
+      data.keys.each do |set|
+        next unless data[set] == 1
+        min_hashes = @redis.hgetall("hmhll:minhash:#{set}").map do |hash_num, minvalue|
+          [hash_num, hash_val[hash_num.to_i] < minvalue.to_i ? hash_val[hash_num.to_i] : minvalue ]
+        end
+        @redis.hmset "hmhll:minhash:#{set}", *min_hashes.flatten
+      end
     end
 
     def similarity(s1, s2)
+      s1_vals = @redis.hvals("hmhll:minhash:#{s1}").map(&:to_i)
+      s2_vals = @redis.hvals("hmhll:minhash:#{s2}").map(&:to_i)
+      #puts s1_vals.to_s
+      #puts s2_vals.to_s
+
+      y = s1_vals.zip(s2_vals).inject(0) { |t, e| t += (e[0] == e[1] ? 1 : 0) }
+      k = s1_vals.length
+      return y.to_f / k
     end
   end
 
   class Estimote
-    include ::Storage::RubyList
+    include ::Storage::Redis
 
     def initialize(keys, num_hashes = 300)
       @data_count = 0
@@ -114,12 +138,13 @@ def print(datum, store, s1, s2)
   puts "#{s1} : #{s2}"
 
   inter = intersection(datum, s1, s2)
-  sim = inter.to_f / union(datum, s1, s2)
+  union = union(datum, s1, s2)
+  sim = inter.to_f / union.to_f
   est_sim = store.similarity(s1, s2)
-  est_inter = store.similarity(s1, s2) * union(datum, s1, s2)
+  est_inter = est_sim * union
 
   puts "inter    : #{inter}"
-  puts "union    : #{union(datum, s1, s2)}"
+  puts "union    : #{union}"
   puts "sim      : #{sim}"
   puts "est sim  : #{est_sim}"
   puts "est inter: #{est_inter}"
@@ -135,7 +160,7 @@ datum.each do |data|
   store.add(data)
 end
 
-print(datum, store, :s1, :s2)
+#print(datum, store, :s1, :s2)
 print(datum, store, :s1, :s3)
 print(datum, store, :s1, :s4)
 print(datum, store, :s2, :s3)
